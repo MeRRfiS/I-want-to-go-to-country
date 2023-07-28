975fbae69edb4d4ba492e8302adaf56faa37dc25
Egor <uit.merrfis@gmail.com>
2023-07-28 14:43:42 +0300

    Changed version

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using static UnityEditor.Progress;
-using static UnityEngine.Rendering.DebugUI;
 
 public class InventoryController : MonoBehaviour
 {
