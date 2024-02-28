99370c18c1a3509ca7739039c7431de3a7a74bb8
MeRRfiS <uit.merrfis@gmail.com>
2024-02-28 16:40:56 +0200

    Change version

--- Assets/Scripts/TreesController.cs
+++ Assets/Scripts/TreesController.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class TreesController : MonoBehaviour
 {
